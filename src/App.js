77531876bdb1cee49c4755031f8d2eadf8247754
Duc Ta <tminhduc2811@gmail.com>
2020-06-18 16:45:34 +0700

    example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+
+class App extends React.Component {
+  state = {
+    content: null,
+  }
+  handleEditorChange = (e) => {
+    const data = JSON.stringify(e.target.getContent());
+    console.log(data)
+  }
+
+  render() {
+    return (
+      <Editor
+        apiKey='83ip6b10skds6fav2qt1j6bp1dcubqwl8yr0lav5k3hjzy5y'
+        initialValue="<p>Initial content</p>"
+        init={{
+          height: 500,
+          menubar: true,
+          plugins: [
+            'advlist autolink lists link image',
+            'charmap print preview anchor help',
+            'searchreplace visualblocks code',
+            'insertdatetime media table paste wordcount'
+          ],
+          image_dimensions: false,
+          image_class_list: [
+            { title: 'Responsive', value: 'img-fluid' },
+            {title: 'None', value: null}
+          ],
+          toolbar:
+            'undo redo | formatselect | bold italic | \
+            alignleft aligncenter alignright | \
+            bullist numlist outdent indent | help'
+        }}
+        onChange={this.handleEditorChange}
+      />
+    );
+  }
 }
 
 export default App;
